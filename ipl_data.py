393100d0a61c7c0e525a69a814fe95f1cc83e2dd
divyajain0706 <divyabelokar170@gmail.com>
2025-06-15 16:55:58 +0530

    Displayed the concise information about the dataset

--- ipl_data.py
+++ ipl_data.py
@@ -3,4 +3,7 @@ import numpy as np
 
 #Loading the CSV file
 df = pd.read_csv(r"D:\Python Programs\pandas programs\IPL data project\matches.csv", encoding="latin1")
-print(df.head(10))
+
+#Displaying the concise information about the dataset
+print("Concise information about the dataset :")
+print(df.info())
